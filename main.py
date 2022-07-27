94e75921d2e818517e60c08cdb9f6cc4446dc8ac
minecon099 <74718722+minecon099@users.noreply.github.com>
2022-07-26 22:13:23 -0500

    New modules! Also new future RP model woah (Update main.py)

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ bot = Bot(command_prefix="ex!")
 #Disable extensions with commentaries.
 bot.load_extension("images")
 bot.load_extension("fun")
-#bot.load_extension("presence")
+bot.load_extension("presence")
 #bot.load_extension("music")
+#bot.load_extension("roleplay")  A wild new extension has appeared! - Can you guess what we are using?
 
 bot.run(TOKEN)
