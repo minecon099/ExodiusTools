bb938d5affe60f1caa6e618cdd6f72682b2eb110
minecon099 <74718722+minecon099@users.noreply.github.com>
2022-07-26 22:10:11 -0500

    Tweakin' and addin a New one! - (Update fun.py cog)

--- fun.py
+++ fun.py
@@ -2,8 +2,9 @@ import random
 import randfacts
 import aiohttp
 
-from nextcord import Interaction, slash_command, embeds
+from nextcord import Interaction, slash_command, embeds, SlashOption, VoiceChannel #Ignore VoiceChannel if you want, it will be used in the future though.
 from nextcord.ext.commands import Bot, Cog
+from nextcord.ext import activities #Trust me, you'll need this in a future update.
 from dadjokes import Dadjoke
 
 print("Module fun.py loaded.")
@@ -34,7 +35,7 @@ class fun(Cog):
         description="Magic 8-Ball, ask a question and get a random answer.",
         default_member_permissions=None,
     )
-    async def eightball(self, inter: Interaction) -> None:
+    async def eightball(self, inter: Interaction, question: str) -> None:
         answers = ['It is certain.',
         'It is decidedly so.',
         'Without a doubt.',
@@ -55,21 +56,22 @@ class fun(Cog):
         'Outlook not so good.',
         'Very doubtful.']
 
-        await inter.send(f"🎱 {random.choice(answers)}")
+        embed8Ball = embeds.Embed(title="Exodius Bot's Magic 8 Ball:", color=0xe303fc)
+        embed8Ball.add_field(name="Your question:",value=question,inline=False)
+        embed8Ball.add_field(name="Answer:",value=f"🎱 {random.choice(answers)}",inline=False)
+        await inter.send(embed=embed8Ball)
     
     @slash_command(
         name="choose",
         description="I'll choose a random between number 1 and the number of your choice.",
         default_member_permissions=None,
     )
-    async def choose(self, inter: Interaction, max_number: int) -> None:
-        if max_number is None:
-            await inter.send("You didn't choose a number.")
-        elif max_number <= 0:
+    async def choose(self, inter: Interaction, max_number: int = SlashOption(description="The highest number to choose (Cannot be greater than 10000).")) -> None:
+        if max_number <= 0:
             await inter.send("You can't choose a negative number.")
         elif max_number <= 1:
             await inter.send("You can't choose a number less than 1.")
-        elif max_number >= 10000:
+        elif max_number >= 10001:
             await inter.send("You can't choose a number greater than 10000.")
         else:
             await inter.send(f"🎲 I choose {random.randint(1, max_number)}")
@@ -79,7 +81,7 @@ class fun(Cog):
         description="Get a random fact from anything at all. Really, anything.",
         default_member_permissions=None,
     )
-    async def randfact(self, inter: Interaction) -> None:
+    async def randomfact(self, inter: Interaction) -> None:
         await inter.send(randfacts.get_fact())
     
     @slash_command(
@@ -93,14 +95,36 @@ class fun(Cog):
                 data = await resp.json()
 
                 rock_name = data["name"]
-                rock_desc = data["description"]
+                rock_desc = data["desc"]
                 rock_image = data["image"]
 
                 embed = embeds.Embed(title=rock_name, description=rock_desc, color=0xe303fc)
                 embed.set_image(url=rock_image)
 
                 await inter.send(embed=embed)
-                
+    
+    @slash_command(
+        name="randomcolor",
+        description="Get a random color in RGB and HEX code.",
+        default_member_permissions=None,
+    )
+    async def randomColor(self, inter: Interaction) -> None:
+
+        #Get random RGB values
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+
+        #Convert to HEX
+        hex_color = f"{r:02x}{g:02x}{b:02x}" #string
+        
+
+        #Display RGB and HEX values in an Embed and make the embed's color with the color generated
+        ColorEmbed = embeds.Embed(color=int(hex_color, 16))
+        ColorEmbed.add_field(name="HEX", value=f"#{hex_color}", inline=False)
+        ColorEmbed.add_field(name="RGB", value=f"{r}, {g}, {b}", inline=False)
+
+        await inter.send(embed=ColorEmbed)
 
 def setup(bot: Bot) -> None:
     bot.add_cog(fun(bot))
